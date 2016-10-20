3540838269cd270da6cc468aa4667e7a534b47a3
joejcollins <joejcollins@gmail.com>
2016-10-20 20:26:53 +0100

    Tests for helpers_geo.py.
    Unfortunately the Google Maps API (http://maps.googleapis.com/maps/api/geocode/json) returns a different result to the Google Web Search.  So for A La Ronde, Devon is returns the centroid for Devon.
    
    e.g. AssertionError: -3.530875 != -3.4092041

--- wiki_scrape/src/helpers_geo.py
+++ wiki_scrape/src/helpers_geo.py
@@ -13,9 +13,9 @@ import simplejson
 data = None
 headers = { 'User-Agent' : 'HeathMynd (+http://www.blackradley.com/contact-us/)' }
 
-def get_wiki_location(link):
-    link = urllib.urlencode(link)
-    kml_url = 'http://toolserver.org/~para/cgi-bin/kmlexport?article=' + link.replace('&', '%26')
+def get_wikipedia_location(wikipedia_link):
+    wikipedia_link = urllib.quote_plus(wikipedia_link)
+    kml_url = 'http://toolserver.org/~para/cgi-bin/kmlexport?article=' + wikipedia_link
     kml_request = urllib2.Request(kml_url, data, headers)
     kml_response = urllib2.urlopen(kml_request)
     kml = kml_response.read()
@@ -29,12 +29,11 @@ def get_wiki_location(link):
     return [lat, lng]
 
 def get_google_location(name, county):
-    address = (name + ', ' + county).replace(' ', '+').replace('&', '%26')
-    google_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false&bounds=49.766807,-7.557160|56.474628,3.493652&region=uk'
-    # restricted by region and bounding box 
-    # 49.766807,-7.557160 is SV00 in the OS Grid (the origin)
-    # 56.474628,3.493652 is somewhere in the North Sea
-    
+    # e.g. http://maps.googleapis.com/maps/api/geocode/json?address=Basildon%20Park,%20Berkshire&bounds=49.766807,-7.557160|56.474628,3.493652&region=uk
+    address_string = (name + ', ' + county).replace(' ', '+')
+    boundary_box_string =  __uk_boundry_box()
+    google_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address_string + '&bounds=' + boundary_box_string + '&region=uk'
+    print google_url
     google_request = urllib2.Request(google_url, data, headers)
     google_response = urllib2.urlopen(google_request)
     json = simplejson.loads(google_response.read())
@@ -49,3 +48,12 @@ def get_google_location(name, county):
     else:
         location = [0.0,0.0]
     return location
+
+def __uk_boundry_box():
+    # SV00 in the OS Grid (the origin)
+    south_west_lat = 49.766807
+    south_west_lng = -7.557160 
+    # Somewhere in the North Sea
+    north_east_lat = 56.474628
+    north_east_lng = 3.493652 
+    return str(south_west_lat) + ',' + str(south_west_lng) + '|' + str(north_east_lat) + ',' + str(north_east_lng)
--- wiki_scrape/src/test_helpers_geo.py
+++ wiki_scrape/src/test_helpers_geo.py
@@ -27,28 +27,37 @@ class test_geo_helpers(unittest.TestCase):
                          ]
 
         self.test_data = [
-                            {
+                            { # Has wiki text but not geocoded
                                 'name': 'Basildon Park',
                                 'wikipedia_link': 'Basildon_Park',
                                 'county': 'Berkshire',
                                 'location_wikipedia': {'lat': 0.0, 'lng': 0.0},
                                 'location_google': {'lat': 51.499099,'lng': -1.121399}
+                            },
+                            { # Has wiki text and is geocoded
+                                'name': 'A La Ronde',
+                                'wikipedia_link': 'A_La_Ronde',
+                                'county': 'Devon', 
+                                'location_wikipedia': {'lat': 50.6417, 'lng': -3.4088},
+                                'location_google': {'lat': 50.7155591, 'lng': -3.4092041}, 
                             }
         ]
       
     def test_get_wiki_location(self):
-        print 'Wikipedia Location Tests'
+        print '\nWikipedia Location Tests\n---'
         for museum in self.test_data:
-            location = helpers_geo.get_wiki_location(museum[1])
-            print museum[0] + str(location)
-            self.assertAlmostEqual(location, museum[3])
+            print museum['name'] + ', ' + museum['wikipedia_link']
+            location = helpers_geo.get_wikipedia_location(museum['wikipedia_link'])
+            self.assertAlmostEqual(location[0], museum['location_wikipedia']['lat'], 4)
+            self.assertAlmostEqual(location[1], museum['location_wikipedia']['lng'], 4)
             
-    def test_get_google_location(self):
-        print 'Google Location Tests' 
+    def test_get_google_maps_location(self):
+        print '\nGoogle Location Tests\n---'
         for museum in self.test_data:
-            location = helpers_geo.get_google_location(museum[0], museum[2])
-            print museum[0] + str(location)
-            self.assertAlmostEqual(location, museum[4])
+            print museum['name'] + ', ' + museum['county']
+            location = helpers_geo.get_google_location(museum['name'], museum['county'])
+            self.assertAlmostEqual(location[0], museum['location_google']['lat'], 4)
+            self.assertAlmostEqual(location[1], museum['location_google']['lng'], 4)
 
 if __name__ == '__main__':
     unittest.main()    
