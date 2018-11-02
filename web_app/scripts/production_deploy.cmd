e4f9a17fe6683f2e5297869dfc51926b3247e760
Joe Colllins <joejcollins@gmail.com>
2018-11-02 13:38:14 +0000

    New layout...nearly

--- web_app/scripts/production_deploy.cmd
+++
@@ -1,4 +0,0 @@
-::
-:: Deploy to GAE
-::
-C:\Python27\python "c:\Program Files\Google\google_appengine\appcfg.py" update ../src
---
+++ web_production_deploy.cmd
@@ -0,0 +1,6 @@
+::
+:: Deploy to GAE
+::
+
+c:\python27\python "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine\appcfg.py"^
+ update ./web_app/
