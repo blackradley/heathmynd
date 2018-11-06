b9420e18294c858eb21ec3bce27290f886e96b42
Joe Colllins <joejcollins@gmail.com>
2018-11-06 12:50:57 +0000

    Static files for whole website

--- web_app/web_app/settings.py
+++ web_app/web_app/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # this is the directory in each app
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+] # this the directory shared by the whole website
