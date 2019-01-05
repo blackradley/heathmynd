e09863d09b7cadb697970e6371caf30747fd71f6
Joe J Collins <joejcollins@users.noreply.github.com>
2019-01-05 10:17:13 +0000

    Serving static files in dev

--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^cheltenham/', include('cheltenham.urls')),
@@ -27,4 +29,4 @@ urlpatterns = [
         lambda x: HttpResponse("User-Agent: *\nDisallow:", content_type="text/plain"),
         name="robots_file"),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
